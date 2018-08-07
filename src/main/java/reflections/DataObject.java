cb61b9b4d22a1709f6e178223a6b10dcc3282637
msdhillon8989 <msdhillon8989@gmail.com>
2018-08-07 16:29:03 +0530

    merge two objects using reflection

--- src/main/java/reflections/DataObject.java
+++ src/main/java/reflections/DataObject.java
@@ -1,4 +1,74 @@
 package reflections;
 
+import java.util.Date;
+
 public class DataObject {
+    private Long id;
+    private String name;
+    private Date createDate;
+    private String mobile;
+    private String address;
+
+    DataObject() {
+
+    }
+
+    public DataObject(Long id, String name, String mobile, String address, Date createDate) {
+        this.id = id;
+        this.name = name;
+        this.mobile = mobile;
+        this.address = address;
+        this.createDate = createDate;
+    }
+
+    public Date getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(Date createDate) {
+        this.createDate = createDate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "DataObject{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", createDate=" + createDate +
+                ", mobile='" + mobile + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
--- src/main/java/reflections/MergeObjects.java
+++ src/main/java/reflections/MergeObjects.java
@@ -1,4 +1,36 @@
 package reflections;
 
+import java.lang.reflect.Field;
+import java.util.Date;
+
 public class MergeObjects {
+    public static void main(String[] args) {
+        DataObject object1 = new DataObject(3L, "Maninder", null, "Address",new Date());
+        DataObject object2 = new DataObject(3L, null, "8660320660", null,null);
+
+        DataObject updated = (DataObject) mergeObject(object1, object2);
+
+        System.out.println(updated.toString());
+    }
+
+    private static Object mergeObject(Object dbObject, Object updates) {
+
+        Field[] declaredFields = updates.getClass().getDeclaredFields();
+
+        for (Field field : declaredFields) {
+            field.setAccessible(true);
+            try {
+                Object value = field.get(updates);
+                if (value == null) {
+                    field.set(updates, field.get(dbObject));
+                }
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            }
+        }
+        return updates;
+    }
 }
+
+
+
